f18bf8db8a4554d98519675caf19e9c50b644bc0
Mert Ulunehir <mertulunehir@gmail.com>
2023-08-16 15:34:05 +0300

    Update SceneInstaller.cs

--- Assets/_Main/Scripts/SceneInstaller.cs
+++ Assets/_Main/Scripts/SceneInstaller.cs
@@ -5,9 +5,9 @@ public class SceneInstaller : MonoInstaller
 {
     public BoardMatchController matchController;
     public BoardCreateController createController;
+
     public override void InstallBindings()
     {
-        Debug.Log("asdas");
         Container.BindInstance(matchController).AsSingle();
         Container.BindInstance(createController).AsSingle();
     }
